10ce990ee72a3bfb77a12cae83d4856404ef0e74
ronanduddy <ronanduddy@live.ie>
2021-01-25 14:03:04 +0000

    Revamp installer.sh
    Copying extra files:
    * CODE_OF_CONDUCT.md
    * LICENCE.md
    * Gemfile.lock
    
    Build command which will initialise the project with
    extra gems such as devise, rspec and set them up.
    
    Generate a database schema before cleaning up all the
    generated docker artifacts.

--- installer.sh
+++ installer.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
 
-# make directory for rails app
+# make directory for the rails app
 mkdir app
 
 # copy important files to app directory
-cp docker-compose.yml Dockerfile entrypoint.sh Gemfile app
+cp docker-compose.yml Dockerfile entrypoint.sh Gemfile Gemfile.lock app
+cp CODE_OF_CONDUCT.md LICENCE.md app
 
-# copy makefile
+# copyname Makefile.app
 cp Makefile.app app/Makefile
 
-# run `rails new .` in app directory (populate with files)
-(cd app && docker-compose run --rm --no-deps web rails new . --force --database=postgresql)
+# Install and setup commands
+RAILS_INSTALL="rails new . --skip-test --skip-system-test --force --database=postgresql;"
+ADD_GEMS="bundle add pry --skip-install -g=development,test;"
+ADD_GEMS+="bundle add rspec-rails --skip-install -g=development,test;"
+ADD_GEMS+="bundle add guard --skip-install -g=development,test;"
+ADD_GEMS+="bundle add guard-rspec --skip-install -g=development,test;"
+ADD_GEMS+="bundle add factory_bot_rails --skip-install -g=development,test;"
+ADD_GEMS+="bundle add database_cleaner-active_record --skip-install -g=test;"
+ADD_GEMS+="bundle add devise --skip-install;"
+BUNDLE="bundle install;"
+INIT_RSPEC="rails generate rspec:install;"
+INIT_GUARD="guard init;"
+INIT_DEVISE="rails generate devise:install; rails generate devise User;"
+CMD="$RAILS_INSTALL$ADD_GEMS$BUNDLE$INIT_RSPEC$INIT_GUARD$INIT_DEVISE"
+
+# Run commands
+(cd app && docker-compose run --rm --no-deps web bash -c "${CMD}")
 
 # on ubuntu; change permissions
 sudo chown -R $USER:$USER app
 
-# copy database config
 cp database.yml app/config
 
-# rebuild image (creates dangling '<none>'-named image...) and create db
-(cd app && docker-compose build && docker-compose run --rm web rake db:create)
+# Apply changes with a re-build and migrate for initial schema generation
+(
+  cd app &&
+  docker-compose build web &&
+  docker-compose run --rm web bash -c "rails db:create db:migrate"
+)
+
+# now that all is generated, clean docker images, containers and volumes
+make docker_clean
+
+echo "Completed!"
+echo "Copy/move and rename your Dockerised Ruby on Rails application found in app/"
+echo "Finally run the following command in your new Rails app directory to boot it up:"
+echo "make setup"
